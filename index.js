4ca390313aaf00b4b1cefedb2b1831a9a29acb3a
Alisher Nurmatov <118418648+AliNur17@users.noreply.github.com>
2024-06-02 14:58:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Showing timestamp
+app.get('/api/', (req, res) => {
+  res.json({unix: new Date().getTime(), utc: new Date().toUTCString()});
+});
+
+app.get('/api/:date_string', (req, res) => {
+  const {date_string} = req.params;
+  let date = new Date(date_string);
+
+  if(date.toString() === 'Invalid Date')
+  {
+    date = new Date(parseInt(date_string));
+  }
+  if(date.toString() === 'Invalid Date'){
+    return res.json({error: "Invalid Date"});
+  }
+  else
+    return res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
